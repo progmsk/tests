513aca22bb1f802af1baebdab091a58b888ed939
skrivenko <skrivenko@users.noreply.github.com>
2021-09-14 12:56:24 +0300

    Простая реализация возведения в степень

--- lesson1/java/src/msk/prog/Power.java
+++ lesson1/java/src/msk/prog/Power.java
@@ -1,2 +1,13 @@
-package msk.prog;public class Power {
+package msk.prog;
+
+import java.math.BigInteger;
+
+public class Power {
+    public static BigInteger calculate(int base, int exp) {
+        BigInteger result = BigInteger.valueOf(1);
+        for (int i = 0; i < exp; i++) {
+            result = result.multiply(BigInteger.valueOf(base));
+        }
+        return result;
+    }
 }
