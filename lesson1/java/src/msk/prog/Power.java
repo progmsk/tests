0ab2e2605493886307310ee62cfc0dcef7bbe578
skrivenko <skrivenko@users.noreply.github.com>
2021-09-14 13:40:27 +0300

    Немного стилей и рефакторинга

--- lesson1/java/src/msk/prog/Power.java
+++ lesson1/java/src/msk/prog/Power.java
@@ -3,21 +3,27 @@ package msk.prog;
 import java.math.BigInteger;
 
 public class Power {
+
     public static BigInteger calculate(int base, Exponent exp) {
         BigInteger result = BigInteger.valueOf(1);
 
         BigInteger newBase = BigInteger.valueOf(base);
         int newExp = exp.getValue();
-        while(newExp > 0) {
-            if (newExp %2 == 0) {
+
+        while (newExp > 0) {
+            if (isEven(newExp)) {
                 newBase = newBase.multiply(newBase);
                 newExp /= 2;
             } else {
                 result = result.multiply(newBase);
-                newExp --;
+                newExp--;
             }
         }
 
         return result;
+    }
+
+    public static boolean isEven(int value) {
+        return value % 2 == 0;
     }
 }
